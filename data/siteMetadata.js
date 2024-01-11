76e4426ca20ce76a89ca24906b4d3db48e085692
lihaixing <lihaixingabc@126.com>
2024-01-11 11:31:17 +0800

    fix: update nextjs version 14.0.4 and siteMetadata modifi

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,9 +1,9 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: '漏油的Blog',
+  author: '漏油',
+  headerTitle: 'LOUYOU_BLOG',
+  description: 'Nothing is impossible',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
